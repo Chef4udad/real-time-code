2357c68769a1de96249a5b7451b62b04c0c6d44b
Chef4udad <harshsharmaca10@gmail.com>
2023-11-06 19:04:40 +0530

    commit

---
+++ src/socket.js
@@ -0,0 +1,18 @@
+import { io } from 'socket.io-client';
+
+export const initSocket = async () => {
+  const options = {
+    'force new connection': true,
+    reconnectionAttempt: 'Infinity',
+    timeout: 10000,
+    transports: ['websocket', 'polling'],
+  };
+
+  // console.log('socket');
+  // just for vercel deployment, Trying socket connection without providing link
+
+  // const vercel_deployment_link = process.env.REACT_APP_VERCEL_URL;
+  // console.log(vercel_deployment_link, ': vercel deployment link ');
+
+  return io(options);
+};
